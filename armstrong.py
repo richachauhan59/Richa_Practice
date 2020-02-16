c34e4083034919447ec56fd4c09c7f2fcb7797cd
Hp\ashpratap007 <pratap.ashishpratap.ashish@gmail.com>
2020-02-16 21:34:31 +0530

    Armstrong Working

--- armstrong.py
+++ armstrong.py
@@ -1,12 +1,13 @@
 num = input()
 temp = 0
 for i in range(0, len(num)):
-    print("i", i)
+    # print("i", i)
     c = int(num[i]) * int(num[i]) * int(num[i])
-    print("C", c)
+    # print("C", c)
     temp += c
-    print("Temp", temp)
-if temp == num:
+#     print("Temp", temp)
+# print(type(temp), type(num))
+if temp == int(num):
     print("armstrong")
 
 else:
